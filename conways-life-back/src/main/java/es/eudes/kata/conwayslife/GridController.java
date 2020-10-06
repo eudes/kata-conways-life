aaf52475ef513b72d5c7c8e87217bea6d91b62a7
eudes <eudes.ochoa@gmail.com>
2020-10-06 11:11:32 +0100

    Controller

---
+++ conways-life-back/src/main/java/es/eudes/kata/conwayslife/GridController.java
@@ -0,0 +1,24 @@
+package es.eudes.kata.conwayslife;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class GridController {
+
+    @CrossOrigin
+    @GetMapping("/init")
+    public String init(int x, int y){
+        log.info("Received {}, {}", x, y);
+        return "{\"grid\": [[1],[1]]}";
+    }
+
+    @CrossOrigin
+    @PostMapping("/next")
+    public String init(@RequestBody String grid){
+        log.info("Received {}", grid);
+        return "{\"grid\": [[1],[1]]}";
+    }
+}
